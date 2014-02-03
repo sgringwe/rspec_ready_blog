4c590af69b36def393242c274cd310fa5256485f
Scott Ringwelski <sgringwe@mtu.edu>
2014-02-03 16:29:58 -0500

    initial commit. added test group gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RspecReadyBlog::Application.config.secret_key_base = '8ad478aaac4c7c1b1d362efb0ccf48b7d2762ddc0a1fb74013e7055b780b61422a0acc97b8849e504c23d4e8930f4ae939df436f97ef021eb66e01ecc71afb8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RspecReadyBlog::Application.config.session_store :cookie_store, key: '_rspec_ready_blog_session'
